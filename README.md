a1fa71609758cdddae41cb1b5fa004eef9262bd5
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2021-11-14 18:01:15 +0100

    Setup mocha, jest and jasmine; added firestore for rest api

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Project Testing Frameworks
- Comparing Mocha, Jest and Jasmine via Express App
+
+Comparing Mocha, Jest and Jasmine via Express App
+
+## Setup
+
+If you are not already in the app folder input "cd app" in the terminal to change the folder.
+
+Then you can start the server with the command "npm start".
--- app/app.js
+++ app/app.js
@@ -1,19 +1,40 @@
-//Setup Server
-const express = require("express");
+//Setup express and firebase
+import express from 'express';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc, getDoc, doc } from 'firebase/firestore';
+
+//env setup
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 const port = 3000;
+
+//Firebase Config
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.PROJECT_NAME + ".firebaseapp.com",
+  projectId: process.env.PROJECT_ID,
+};
+
+//start firestore
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+
+
 
 //Setup pug
 app.set('view engine', 'pug');
 
 app.get("/", (req, res) => {
-  res.send("Hello World.This is my first route in express!")
+  res.send("Hello there main page!")
 });
 
 app.get("/pug", function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!'});
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
 // Start listening on port 3000.
-app.listen(port, () => 
-console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`));
+
