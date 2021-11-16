4ffbf99cd2638183ca442239028831083a16e96c
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2021-11-16 12:47:46 +0100

    Added CRUD operations for firebase interaction and updated the documentation for basic setup

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Comparing Mocha, Jest and Jasmine via Express App
 
 ## Setup
 
-If you are not already in the app folder input "cd app" in the terminal to change the folder.
+If you are not already in the /app folder then change into the /app folder.
 
-Then you can start the server with the command "npm start".
+Install dependencies via "npm install".
+
+To use firebase copy the ".env.example" and rename it to .env.
+
+Then input your firebase credentails in the .env file and save it.
+
+To start the project use "npm start"
+
+And open service on address: http://localhost:3000
--- app/app.js
+++ app/app.js
@@ -1,14 +1,18 @@
 //Setup express and firebase
 import express from 'express';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, addDoc, getDoc, doc } from 'firebase/firestore';
+import { getFirestore, doc, setDoc, getDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 //env setup
 import dotenv from 'dotenv';
+import { async } from '@firebase/util';
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+//Setup pug
+app.set('view engine', 'pug');
 
 //Firebase Config
 const firebaseConfig = {
@@ -17,17 +21,59 @@ const firebaseConfig = {
   projectId: process.env.PROJECT_ID,
 };
 
-//start firestore
-const firebaseApp = initializeApp(firebaseConfig);
+//start firestore implementation and get the database
+initializeApp(firebaseConfig);
 const db = getFirestore();
 
+//Setup collection and document
+const collectionName = "cities";
+const docName = "LA"
+const docRef = doc(db, collectionName, docName);
+const docSnap = await getDoc(docRef);
 
 
-//Setup pug
-app.set('view engine', 'pug');
 
 app.get("/", (req, res) => {
   res.send("Hello there main page!")
+});
+
+app.post("/create", async (req, res) => {
+
+  //create document if document doesn't exist already
+  if (!docSnap.exists()) {
+    await setDoc(doc(db, collectionName, docName), {
+      name: "Los Angeles",
+      state: "CA",
+      country: "USA"
+    });
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("Document exists already!");
+  }
+  res.status("200").send(`Created a new collection with name:${collectionName}`);
+});
+
+app.get("/read", (req, res) => {
+
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
+  res.status("200").send(`Read collection with name:${collectionName}`);
+});
+
+app.put("/update", async (req, res) => {
+  await updateDoc(docRef, {
+    capital: true
+  })
+  res.status("200").send(`Updated collection with name:${collectionName}`);
+});
+
+app.delete("/delete", async (req, res) => {
+  await deleteDoc(docRef)
+  res.status("200").send(`Deleted collection with name:${collectionName}`);
 });
 
 app.get("/pug", function (req, res) {
