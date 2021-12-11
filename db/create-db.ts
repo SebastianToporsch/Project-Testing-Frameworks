90d9e61ccd31ef5f59f1b71becfa8d6d6390e61c
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2021-12-11 01:11:03 +0100

    when creating the database also creates a table now

--- db/create-db.ts
+++ db/create-db.ts
@@ -1,25 +1,70 @@
 import mysql from 'mysql';
 import dotenv from 'dotenv';
-dotenv.config({path:"config/.env" });
+dotenv.config({ path: "config/.env" });
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
-  user:  process.env.DATABASE_USER,
-  password:  process.env.DATABASE_PASSWORD,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   multipleStatements: true
 });
 
 // log if an error occurs
-connection.on('error', function() {
+connection.on('error', function () {
   connection.destroy();
-  console.log('An error occured while creating the database');
+  console.log('An ERROR occured!');
 });
 
-connection.connect(function(err: any) {
-  if(err) throw err;
-  connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DATABASE_NAME};`, (err:any)=> {
+connection.connect(function (err: any) {
+  if (err) throw err;
+  // tslint:disable-next-line: no-shadowed-variable
+  connection.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DATABASE_NAME};`, (err: any) => {
     if (err) throw err;
     console.log("Database created");
     connection.destroy();
+    createTable();
   });
 });
+
+function createTable() {
+  // sql for creating tables
+  const sql_user = `
+  CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    username varchar(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT UNIQ_username UNIQUE (username)
+  );`;
+  // create connection to the database
+  connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    multipleStatements: true
+  });
+
+  // log exception
+  connection.on('error', function () {
+    connection.destroy();
+    console.log('An ERROR occured!');
+  });
+
+  // connect to the database
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    // execute the sql string
+    // tslint:disable-next-line: no-shadowed-variable
+    connection.query(sql_user, function (err: any, result: any) {
+      if (err) throw err;
+      console.log("Tables created");
+      connection.destroy();
+    });
+  });
+}
+
+
+
+
+
