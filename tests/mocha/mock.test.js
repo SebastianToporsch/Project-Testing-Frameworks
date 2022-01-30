27e98c01e18fb5d6816494671db0f9e76048c6c4
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2022-01-30 02:23:56 +0100

    removed unecessary functions and mocks

--- tests/mocha/mock.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('superagent');
-require('dotenv').config({ path: 'config/.env' });
-const rest_port = process.env.REST_PORT || 3000;
-const baseURL = `http://localhost:${rest_port}`
-
-describe('Name of the group', () => {
-  it('should behave...', () => {
-  });
-});
